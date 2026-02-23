39601baffab9c358ec33afd8fa34e12043dc4003
Nadiia Poshtova <feyanana29@gmail.com>
2026-02-23 17:23:55 +0200

    fix header

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
 	return (
 		<header className={styles.header} style={{ backgroundImage: `url(${backgroundImage})` }}>
 			<div className={styles.wrapper}>
-				<h1>Порфоліо - Надії Поштової</h1>
+				<h1>Портфоліо - Надії Поштової</h1>
 				<div className={styles.ava_block}></div>
 
 				<nav className={styles.nav_header}>
